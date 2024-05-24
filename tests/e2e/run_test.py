97c6d275ba5ce40837470a0dae5ff80b4a68cd0c
longbiao.kang <longbiao.kang@i-tudou.com>
2024-05-24 14:25:42 +0800

    支持图片问答，基于gpt-4o测试通过

--- tests/e2e/run_test.py
+++ tests/e2e/run_test.py
@@ -11,36 +11,76 @@ def test_create_run_with_additional_messages_and_other_parmas():
     assistant = client.beta.assistants.create(
         name="Assistant Demo",
         instructions="你是一个有用的助手",
-        model="gpt-3.5-turbo-1106",
+        model="gpt-4o",
     )
-    thread = client.beta.threads.create()
-    run = client.beta.threads.runs.create(
+    thread = client.beta.threads.create(
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "这张图片的场景是什么季节？"
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
+                        }
+                    }
+                ]
+            }
+        ],
+    )
+
+    thread_message = client.beta.threads.messages.create(
+        thread.id,
+        role="user",
+        content="这张图片中有人物吗？",
+    )
+
+    stream = client.beta.threads.runs.create(
         thread_id=thread.id,
         assistant_id=assistant.id,
         instructions="",
         additional_messages=[
-            {"role": "user", "content": "100 + 100 等于多少"},
-            {"role": "assistant", "content": "100 + 100 等于200"},
-            {"role": "user", "content": "如果是乘是多少呢？"},
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "这张图片里有什么？"
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
+                        }
+                    }
+                ]
+            }
         ],
         max_completion_tokens=100,
         max_prompt_tokens=100,
         temperature=0.5,
         top_p=0.5,
+        stream=True,
     )
-    query = session.query(Run).filter(Run.id == run.id)
+
+    print("\nstream message start")
+    for event in stream:
+        if event.event == "thread.message.delta":
+            print(event.data.delta.content[0].text.value, end="", flush=True)
+    print("\nstream message finish")
+
+    query = session.query(Run).filter(Run.thread_id == thread.id)
     run = query.one()
     assert run.instructions == "你是一个有用的助手"
-    assert run.model == "gpt-3.5-turbo-1106"
-    query = session.query(Message).filter(Message.run_id == run.id).order_by(Message.created_at)
+    assert run.model == "gpt-4o"
+    query = session.query(Message).filter(Message.thread_id == thread.id).order_by(Message.created_at)
     messages = query.all()
-    [messgae1, messgae2, messgae3] = messages
-    assert messgae1.content == [{"text": {"value": "100 + 100 等于多少", "annotations": []}, "type": "text"}]
-    assert messgae1.role == "user"
-    assert messgae2.content == [{"text": {"value": "100 + 100 等于200", "annotations": []}, "type": "text"}]
-    assert messgae2.role == "assistant"
-    assert messgae3.content == [{"text": {"value": "如果是乘是多少呢？", "annotations": []}, "type": "text"}]
-    assert messgae3.role == "user"
+    for message in messages:
+        print(message)
     assert run.max_completion_tokens == 100
     assert run.max_prompt_tokens == 100
     assert run.temperature == 0.5
